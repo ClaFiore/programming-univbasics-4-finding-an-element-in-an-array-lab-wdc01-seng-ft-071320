6c70ba7c830c6f86a83273a672b5f8a1730811de
Claudia Borghini <claborghini@gmail.com>
2020-06-29 14:12:43 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0
 when count < array.length do
-  if value_to_find in array
-    puts index
-  else
-    return nil
-end
+
+  count += 1
 end
